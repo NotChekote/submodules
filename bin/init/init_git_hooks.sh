e97ba1d1d4ae25f2ea22f7177730c22110e6069e
Donald Tyler <domhnall.tilliere@gmail.com>
2021-11-10 09:29:20 -0600

    Consolidate multiple init_git_hooks scripts (#86)

--- bin/init/init_git_hooks.sh
+++ bin/init/init_git_hooks.sh
@@ -2,32 +2,88 @@
 
 set -euo pipefail
 
-root="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../../../" && pwd )"
+#######################################
+# Exits if the current working directory is not a git repo
+#######################################
+assert_git_repo () {
+  if [[ ! -d .git ]]; then
+    echo "This script must be ran from the root of a git repository";
+    exit 1;
+  fi;
+}
 
-hooks="$root/.git/hooks"
+#######################################
+# Create the hooks symlink
+#######################################
+create_symlink () {
+  echo "Creating symbolic link..."
+  ln -s ../hooks .git/hooks
+}
+
+#######################################
+# Exits if the user does not provide authorization to remove the hooks folder
+#######################################
+exit_if_not_willing_to_remove_dir () {
+  local response
+
+  while true; do
+    read -r -p "Symbolic link is NOT configured correctly. We need to remove the git hooks directory. Is that okay? (y/n)" response
+    case "$response" in
+      [Yy]* ) break;;
+      [Nn]* )  echo "Git hook initialization canceled by user"; exit 1;;
+      * ) echo "Please answer yes or no (y/n).";;
+    esac
+  done
+}
+
+#######################################
+# Removes and creates the hooks symlink
+#######################################
+recreate_symlink () {
+  remove_git_hooks
+  create_symlink
+}
+
+#######################################
+# Removes the git hooks
+#
+# Globals:
+#   CI true if the current env is CI, false if not.
+#######################################
+remove_git_hooks() {
+  if [[ "${CI:-}" != true ]] ; then
+      exit_if_not_willing_to_remove_dir
+  fi
+
+  echo "Removing..."
+  rm -rf .git/hooks
+}
+
+#######################################
+# Verifies that the current hooks symlink is configured correct, and fixes it if not
+#######################################
+verify_symlink() {
+  if [[ "$(readlink .git/hooks)" == ../hooks ]]; then
+    echo "Symbolic link is configured correctly."
+  else
+    recreate_symlink
+  fi
+}
+
+assert_git_repo
 
 echo "Checking Git hooks..."
 
-if [ -L "$hooks" ]; then
-  echo "Git hooks symbolic link is already in place."
-  if [ "$(readlink "$hooks")" == '../hooks' ]; then
-    echo 'Symbolic link is configured correctly.'
-  else
-    echo 'Symbolic link is NOT configured correctly. Removing...'
-    rm "$hooks"
-
-    echo 'Creating symbolic link...'
-    ln -s ../hooks "$hooks"
-  fi
+if [[ -L .git/hooks ]]; then
+  echo "Git hooks symbolic link is already configured correctly."
+  verify_symlink
 else
-  echo 'Git hooks symbolic link is not in place.'
-  if [ -e "$hooks" ]; then
-    echo 'Git hooks dir present. Removing...'
-    rm -rf "$hooks"
+  echo "Git hooks symbolic link is not configured correctly."
+  if [[ -e .git/hooks ]]; then
+    remove_git_hooks
   fi
 
-  echo 'Creating symbolic link...'
-  ln -s ../hooks "$hooks"
+  create_symlink
 fi
 
 echo "Git hooks symbolic link setup is complete."
