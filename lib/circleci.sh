76d87aa3ee428bc6d7bcd69d2acd3122f879e2f2
Bruno Gutierrez Rios <bruno.gutierrez96@gmail.com>
2021-12-22 11:04:16 -0500

    Adding Command circleci.pr.number (#94)

--- lib/circleci.sh
+++ lib/circleci.sh
@@ -37,8 +37,17 @@ circleci.build.oldest() {
     export OLDEST_RUNNING_BUILD_NUM="$oldest"
 }
 
-circleci.build.cancel(){
+circleci.build.cancel() {
     echo "Canceling build $CIRCLE_BUILD_NUM"
     local cancel_api_url_template="https://circleci.com/api/v1.1/project/$vcs_type/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/$CIRCLE_BUILD_NUM/cancel?circle-token=$CIRCLECI_API_KEY"
     curl -s -X POST "$cancel_api_url_template" > /dev/null
+}
+
+#######################################
+# Grabs the PR Number of the current pull request in circleci
+#
+# More consistently available than CIRCLE_PR_NUMBER env variable.
+#######################################
+circleci.pr.number() {
+  echo "${CIRCLE_PULL_REQUEST##*/}"
 }
